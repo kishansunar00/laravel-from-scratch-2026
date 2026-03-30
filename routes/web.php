16404f88db3aca39c51ac8039c67d99b4054de59
Kishan Sunar <kishan@thefireflytech.com>
2026-03-30 17:45:27 +0545

    fix: update welcome page to use default name parameter in request

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome', [
     'greeting' => 'Hello',
-    'person' => request('name') ?? 'Buddy!',
+    'person' => request('name', 'Buddy!'),
     'html' => '<strong>Bold</strong>',
 ]);
 Route::view('/about', 'about');
