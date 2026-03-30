67ce53622a7c6af9d16a0ca9a88c824ed43c3503
Kishan Sunar <kishan@thefireflytech.com>
2026-03-30 17:29:19 +0545

    style: add custom link styles to layout component

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,6 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $title ?? 'Hello Buddy' }}</title>
+    <style>
+        a {
+            color: brown;
+            text-decoration: none;
+            margin-right: 10px;
+        }
+    </style>
 </head>
 <body>
     <nav>
