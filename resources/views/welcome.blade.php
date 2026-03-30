f3c9297d6e187e5a8bae7fc34dcfebec47dc2074
Kishan Sunar <kishan@thefireflytech.com>
2026-03-30 17:43:35 +0545

    feat: enhance welcome page with dynamic HTML rendering

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,10 @@
 <x-layout title="Home">
+    <!-- This is used to display (dump/print) variable values  -->
     <h1>{{ $greeting }} {{ $person }}</h1>
     <p>
         This is the home page.
     </p>
+    <div>
+        {!! $html !!} <!-- is used to render the HTML as plain text -->
+    </div>
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::view('/', 'welcome', [
     'greeting' => 'Hello',
     'person' => request('name') ?? 'Buddy!',
+    'html' => '<strong>Bold</strong>',
 ]);
 Route::view('/about', 'about');
 Route::view('/contact', 'contact');
